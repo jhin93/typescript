da39b6c005edab6e14e3d983c7d2e48d0d45f2de
jhin93 <scene1993@gmail.com>
2022-06-17 17:29:17 +0900

    타입스크립트의 클래스 사용 예제(Dict)

--- Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
+++ Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
@@ -1,29 +1,42 @@
-// 추상 클래스.
-// 추상 클래스는 다른 클래스가 상속받을 수 있는 클래스이다.
-// 하지만 이 클래스는 직접 새로운 인스턴스를 만들 수는 없다.
-abstract class User{
-    constructor( 
-        protected firstName:string,
-        protected lastName:string,
-        protected nickname:string 
-    ) {}
-    // 메소드
-    getFullName(){
-        return `${this.firstName} ${this.lastName}`
-    }
-    // 추상 메소드. 추상 메소드는 구현부(={}내부)를 작성하지 않는다. 그리고 메소드의 call signature만 적어두어야 한다.
-    abstract getNickName():void // 이 추상 메소드는 void를 리턴한다.
+type Words = {
+    [key:string]: string // Words 타입이 string 만을 property로 가지는 오브젝트라는 것을 말해줌. 이것은 제한된 양의 property 혹은 key를 가지는 타입을 정의해주는 방법이다.
+    // property의 이름은 모르지만, 타입만을 알때 쓰는 방식이다.
 }
 
-
-class Player extends User { // player는 User로부터 상속받는다(extends)
-    // 클래스 Player는 추상 메소드 getNickName을 구현해야 한다.
-    getNickName(): void {
-        console.log(this.nickname) // protected이기 때문에 상속을 받으면 nickname에 접근이 가능하다.
+class Dict {
+    private words: Words // words를 initializer 없이 선언하고.
+    constructor() {
+        this.words = {} // constructor에서 수동으로 초기화한다
+    }
+    add(word:Word){
+        if(this.words[word.term] === undefined){
+            this.words[word.term] = word.def
+        }
+    }
+    def(term:string){
+        return this.words[term]
     }
 }
 
-const nico = new Player("nico", "las", "니꼬")
+class Word {
+    constructor (
+        public term:string,
+        public def:string
+    ) {
 
-nico.getFullName() 
-nico.firstName() // firstName은 protected이기 때문에 이런 식으로 외부에서의 접근은 차단된다.
+    }
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+
+const dict = new Dict()
+dict.add(kimchi);
+dict.def("kimchi")
+
+
+
+
+
+
+
+
