dfbc5d086b5eaa74aa4421d0937b7c745dfb2d24
jhin93 <scene1993@gmail.com>
2022-06-17 17:53:54 +0900

    readonly로 수정 방지. static메소드도 자바스크립트처럼 그대로 사용 가능

--- Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
+++ Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
@@ -1,17 +1,16 @@
 
 // 1. 타입을 만드는 방법
 type Words = {
-    [key:string]: string // Words 타입이 string 만을 property로 가지는 오브젝트라는 것을 말해줌. 이것은 제한된 양의 property 혹은 key를 가지는 타입을 정의해주는 방법이다.
-    // property의 이름은 모르지만, 타입만을 알때 쓰는 방식이다. 아무 이름을 가진 키를 사용하는 object이고, string을 가진다.
+    [key:string]: string 
 }
 
 class Dict {
-    // 2. property를 만들고 원하는 대로 초기화 가능
-    private words: Words // words를 initializer 없이 선언됨. property가 constructor부터 바로 초기화 되지 않음
+
+    private words: Words
     constructor() {
-        this.words = {} // constructor에서 수동으로 초기화한다
+        this.words = {}
     }
-    add(word:Word){ // 3. 만든 클래스(Word)를 type 으로 쓸 수 있음
+    add(word:Word){
         if(this.words[word.term] === undefined){
             this.words[word.term] = word.def
         }
@@ -19,18 +18,24 @@ class Dict {
     def(term:string){
         return this.words[term]
     }
+    static hello() {
+        return "hello"
+    }
 }
 
 class Word {
     constructor (
-        public term:string,
-        public def:string
+        public readonly term:string, // 현재 term과 def는 둘다 public 이라서 누군가 기존의 값을 변경할 수 있다. public이지만 변경할 수 없도록 하려면 readonly를 추가한다.
+        public readonly def:string
     ) {
 
     }
 }
 
 const kimchi = new Word("kimchi", "한국의 음식");
+kimchi.def = "xxx" // readonly이기 때문에 값을 "xxx"로 변경하는 것은 불가능하다
+Dict.hello // static 메소드를 자바스크립트처럼 사용 가능. static메소드는 인스턴스 없이 호출이 가능하다
+dict.hello // 인스턴스화(const dict = new Dict())됐기 때문에 static 메소드인 hello를 부를 수 없다.
 
 const dict = new Dict()
 dict.add(kimchi);
