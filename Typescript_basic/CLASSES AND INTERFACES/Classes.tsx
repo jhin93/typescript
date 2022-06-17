77f94e39f273cd7423b662d297851ee6394d5ab1
jhin93 <scene1993@gmail.com>
2022-06-17 15:58:52 +0900

    protected. 상속을 받은 곳에서는 사용이 가능하다

--- Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
+++ Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
@@ -3,9 +3,9 @@
 // 하지만 이 클래스는 직접 새로운 인스턴스를 만들 수는 없다.
 abstract class User{
     constructor( 
-        private firstName:string, // private는 외부, 상속받은 곳에서 사용할 수 없다. 해당 클래스의 인스턴스 혹은 메소드에서 접근할 수 있다. 추상 클래스는 인스턴스도 못만든다.
-        private lastName:string,
-        private nickname:string 
+        protected firstName:string,
+        protected lastName:string,
+        protected nickname:string 
     ) {}
     // 메소드
     getFullName(){
@@ -19,10 +19,11 @@ abstract class User{
 class Player extends User { // player는 User로부터 상속받는다(extends)
     // 클래스 Player는 추상 메소드 getNickName을 구현해야 한다.
     getNickName(): void {
-        console.log(this.nickname) // 상속을 한다 하더라도 private property에는 접근할 수 없다.
+        console.log(this.nickname) // protected이기 때문에 상속을 받으면 nickname에 접근이 가능하다.
     }
 }
 
 const nico = new Player("nico", "las", "니꼬")
 
 nico.getFullName() 
+nico.firstName() // firstName은 protected이기 때문에 이런 식으로 외부에서의 접근은 차단된다.
