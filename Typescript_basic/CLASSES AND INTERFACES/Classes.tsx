cc8c111125b24917615d0cfcccc275de77ba38c6
jhin93 <scene1993@gmail.com>
2022-06-17 20:45:00 +0900

    4.3부터 다시

--- Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
+++ Typescript_basic/CLASSES AND INTERFACES/Classes.tsx
@@ -1,30 +1,24 @@
 
-// interface는 property를 축적하는 것이 가능하다.
-interface User {
-    name:string
-}
-
-interface User {
-    lastname:string
-}
-
-interface User {
-    health: number
-}
-
-// 타입스크립트가 알아서 다 합쳐줌. type은 안됨.
-const nico: User = {
-    name:"nico",
-    lastname:"test",
-    health:10
+abstract class User { // 추상 클래스는 이걸 상속받는 다른 클래스가 가질 property와 메소드를 지정하도록 해줌
+    constructor(
+        protected firstName:string,
+        protected lastName:string
+    ){}
+    abstract sayHi(name:string):string // string으로 된 name을 받아서 string을 반환
+    
 }
 
 
-// type은 interface와 달리 축적(합체)가 안된다.
-type User2 = { // User2' 식별자가 중복되었습니다.
-    name:string
-}
 
-type User2 = {
-    lastName:string
-}
+
+
+
+
+
+
+
+
+
+
+
+
