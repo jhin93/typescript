9ecbc71756d5905cb17aa5f2dcfbef585d772978
jhin93 <scene1993@gmail.com>
2022-07-20 15:59:25 +0900

    제네릭 쉬운 사용법

--- Typescript_basic/CLASSES AND INTERFACES/localstorageAPI.ts
+++ Typescript_basic/CLASSES AND INTERFACES/localstorageAPI.ts
@@ -11,3 +11,25 @@ interface SStorage<T> {
 class LocalStorage<T> { // 1. LocalStorage 클래스를 초기화할 때, T라는 제네릭을 받음. 제네릭을 클래스로 보낸 것
     private storage: SStorage<T> // 2. 클래스가 제네릭을 인터페이스로 보냄
 }
+
+interface Mobile<T> {
+    name: string;
+    price: number;
+    option: T;
+}
+
+const m1: Mobile<object> = { // 아래 option이 객체(object)이기 때문에 T에 object 대입
+    name: "s21",
+    price: 1000,
+    option: {
+        color: "red",
+        coupon: false,
+    },
+};
+
+const m2: Mobile<string> = { // 아래 option이 문자열(string)이기 때문에 T에 string 대입
+    name: "s20",
+    price: 900,
+    option: "good",
+};
+
