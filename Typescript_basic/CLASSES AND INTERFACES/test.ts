5a6ee3a90031a486a528be5bbfc05743ccbff776
jhin93 <scene1993@gmail.com>
2022-07-20 17:40:25 +0900

    에러 해결 과정

--- Typescript_basic/CLASSES AND INTERFACES/test.ts
+++ Typescript_basic/CLASSES AND INTERFACES/test.ts
@@ -16,13 +16,14 @@ const user: User = { name: "a", age: 10};
 const car: Car = { name: "bmw", color: "red"};
 const book: Book = { price: 3000 };
 
-function showName<T>(data: T): string { 
-    return data.name // 실제로 전달되는 user나 car에는 name 속성이 있지만, 모든 매개변수에 name 속성이 있다고 장담할 수 없음
+function showName<T extends { name: string }>(data: T): string { // 매개변수로 들어오는 어떤 T 타입은 name이 string인 객체를 확장한 형태이다. 다양한 모습의 객체가 T로써 올 수 있겠지만, 항상 { name: string } 형태를 가지고 있다
+    return data.name
 }
 
 showName(user);
 showName(car);
-showName(book);
+showName(book); // 만약에 name 속성이 없거나, string이 아니라면 이와같이 에러가 남
+
 // 제네릭 적용 전(위 3개 사례 모두 동일) 결과(shoName 호출 위에 마우스 올렸을 때):
 // function showName(data: any): string
 
