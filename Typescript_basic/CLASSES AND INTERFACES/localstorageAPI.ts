e841331caa1c48569a8759cffc8b5d27a95eb805
jhin93 <scene1993@gmail.com>
2022-07-21 17:07:41 +0900

    [key:string] key가 제한되지 않은 오브젝트를 정의하게 해줌.

--- Typescript_basic/CLASSES AND INTERFACES/localstorageAPI.ts
+++ Typescript_basic/CLASSES AND INTERFACES/localstorageAPI.ts
@@ -1,11 +1,10 @@
 
 
 // Storage는 이미 정의된 예약어(마우스 올려서 확인 가능). interface Storage를 override(속성추가)하지 않고 SStorage 사용 
-// [key:string] - key가 제한되지 않은 오브젝트를 정의하게 해줌.
 
 
-interface SStorage<T> { 
-    [key:string] : T // 3. 인터페이스가 제네릭을 사용
+interface SStorage<T> { // 3. 인터페이스가 제네릭을 사용
+    [key:string] : T // key가 제한되지 않은 오브젝트를 정의하게 해줌. 이게(?) 얼마나 많은진 모르지만, 무슨 타입인지는 안다.
 }
 
 class LocalStorage<T> { // 1. LocalStorage 클래스를 초기화할 때, T라는 제네릭을 받음. 제네릭을 클래스로 보낸 것
