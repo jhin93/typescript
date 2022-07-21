47e4e3f2e18ade936d7347d96152d7dd6f936d55
jhin93 <scene1993@gmail.com>
2022-07-21 17:45:56 +0900

    제네릭을 리턴값의 타입을 정의하는 데 사용하는 예시

--- Typescript_basic/CLASSES AND INTERFACES/localstorageAPI.ts
+++ Typescript_basic/CLASSES AND INTERFACES/localstorageAPI.ts
@@ -3,12 +3,32 @@
 
 
 interface SStorage<T> { // 3. 2에서 인터페이스로 보낸 제네릭을 여기서 받음.
-    [key:string] : T // key가 제한되지 않은 오브젝트를 정의하게 해줌. 이게(?) 얼마나 많은진 모르지만, 무슨 타입인지는 안다. 그리고 받은 T를 위치시킴([key:string] : T)
+    [key:string] : T // key가 제한되지 않은 오브젝트를 정의하게 해줌. 즉, key 값은 정해지지 않았지만, 무슨 타입인지는 안다(string 타입이라는 말). 그리고 받은 T를 위치시킴([key:string] : T)
 }
 
 // 제네릭은 다른 타입에게 물려줄 수 있다.
 class LocalStorage<T> { // 1. LocalStorage 클래스를 초기화할 때, T라는 제네릭을 받음.
-    private storage: SStorage<T> = {} // 2. 이 제네릭은 클래스 이름(LocalStorage<T>)에 들어오지만, 같은 제네릭을 인터페이스(SStorage)로 보내줄 것(SStorage<T>)
+    private storage: SStorage<T> = {} // 2. 이 제네릭은 클래스 이름(LocalStorage<T>)에 들어오지만, 같은 제네릭을 인터페이스(SStorage)로 보내줄 것(SStorage<T>).
+    set(key:string, value:T) { // set 메소드 생성. set 메소드를 쓸 때, string 형식의 key를 넣음. value는 T 형식
+        this.storage[key] = value // storage에 key-value쌍을 저장.
+    } 
+    remove(key:string) { // remove는 string 형식의 key를 받아서 이걸 로컬 스토리지로부터 지움.
+        delete this.storage[key]
+    } 
+    get(key:string):T { // get 메소드. T를 리턴.
+        return this.storage[key]
+    } 
+    clear(){ // clear는 storage를 비움
+        this.storage = {}
+    }
 }
 
 // 제네릭을 클래스로 보냄(LocalStorage<T>) -> 클래스는 제네릭을 인터페이스로 보냄(SStorage<T>) -> 인터페이스는 제네릭을 사용함([key:string] : T)
+
+// class LocalStorage<T> 사용예시
+const stringsStorage = new LocalStorage<string> // const stringsStorage: LocalStorage<string>
+stringsStorage.get("test1") // LocalStorage<string>.get(key: string): string // string을 보내주고. string을 받게 될 것.
+
+
+const numberStorage = new LocalStorage<number>
+numberStorage.get("test2") // LocalStorage<number>.get(key: string): number // string을 보내주고. number을 받게 될 것.
