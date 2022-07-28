4956b2374f1e3bfebc0972966294eb408e9a865b
jhin93 <scene1993@gmail.com>
2022-07-28 17:46:51 +0900

    코드 챌린지 제출 : call signatures

---
+++ Typescript_challenge/assign4.ts
@@ -0,0 +1,12 @@
+/*
+Using what you have learned so far, write the call signatures for two functions:
+
+last(arr): 이 함수는 array의 마지막 아이템을 리턴해야 합니다.
+prepend(arr, item): 이 함수는 array의 시작에 아이템을 넣고, 리턴해야 합니다.
+
+*/
+
+
+type lastE = <T>(arr: T[]) => T
+
+type unshiftArr = <T, M>(a: T[], b: M) => T[]
