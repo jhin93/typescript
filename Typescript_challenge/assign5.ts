cbd9b11eee35651ca301d544a9abf876867263f0
jhin93 <scene1993@gmail.com>
2022-07-29 13:54:38 +0900

    delete 완성

--- Typescript_challenge/assign5.ts
+++ Typescript_challenge/assign5.ts
@@ -1,13 +1,3 @@
-/*
-Typescript Classes 를 사용하여, Dict (dictionary) class 를 빌드하세요. Dict은 아래와 같은 methods 를 갖고있어야 합니다.
-
-add: 단어를 추가함.
-get: 단어의 정의를 리턴함.
-delete: 단어를 삭제함.
-update: 단어를 업데이트 함.
-showAll: dictionary 의 단어를 모두 프린트함.
-count: dict 단어들의 총 count 를 리턴함.
-*/
 
 type Words = {
     [key: string]: string
@@ -32,6 +22,11 @@ class Dict {
     count() {
         return Object.keys(this.words).length
     }
+    deleteWord(word: Word) {
+        if(this.words[word.term] !== undefined){
+            delete this.words[word.term]
+        }
+    }
 
 }
 
@@ -43,8 +38,12 @@ class Word {
 }
 
 const kimchi = new Word("kimchi", "한국의 음식")
+const curry = new Word("curry", "인도의 음식")
+const lemon = new Word("lemon", "과일")
 
 const dict = new Dict()
 
 dict.add(kimchi);
+dict.add(curry);
+dict.add(lemon);
 // dict.get("kimchi")
