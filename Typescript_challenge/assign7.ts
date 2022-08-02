329b7026221998aacf5305fc4f4d1dc933c21233
jhin93 <scene1993@gmail.com>
2022-08-02 13:52:58 +0900

    localStorageAPI 완료

--- Typescript_challenge/assign7.ts
+++ Typescript_challenge/assign7.ts
@@ -1,12 +1,35 @@
 
 
-interface LocalStorageAPI {
-
+interface LocalStorageAPI<T> {
+    [key:string]: T
 }
 
-class LocalStorage {
-    
+abstract class abstractLocalStorage<T> {
+    constructor(
+        protected storage: LocalStorageAPI<T> = {}
+    ) {}
+    abstract setItem(key:string, value:T)
+    abstract getItem(key:string)
+    abstract  clearItem(key:string)
+    abstract clear()
 }
+
+class localStorage<T> extends abstractLocalStorage<T> {
+    setItem(key:string, value:T) {
+        this.storage[key] = value
+    }
+    getItem(key:string) {
+        return this.storage[key]
+    }
+    clearItem(key:string) {
+        delete this.storage[key]
+    }
+    clear() {
+        this.storage = {}
+    }
+}
+
+
 
 
 interface GeolocationAPI {
