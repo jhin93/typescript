68c0665d0c94b18d6ed22b13a35cc12a1e1403b0
jhin93 <scene1993@gmail.com>
2022-08-04 16:23:16 +0900

    5.5 allowJs, JSDoc

--- Typescript_basic/typechain/src/myPackage.js
+++ Typescript_basic/typechain/src/myPackage.js
@@ -1,7 +1,20 @@
-export function ini(config) {
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+ export function init(config) {
     return true;
-}
-
-export function exit(code) {
-    return code + 1
-}
+  }
+  
+  /**
+   * Exits the program
+   * @param {number} code
+   * @returns number
+   */
+  export function exit(code) {
+    return code + 1;
+  }
